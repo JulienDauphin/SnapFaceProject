dccf23085b1762db04ed38a37cff043c7f549f8d
Julien Dauphin <juliendauphin.openclassrooms@gmail.com>
2023-12-19 16:16:52 +0100

    Ajout des photos dynamiquement + modification du style

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FaceSnap } from '../models/face-snap-model';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.scss'
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit{
+@Input() faceSnap!: FaceSnap;
+  buttonText! : string;
 
+    ngOnInit() {
+      this.buttonText = "J'adore";
+    }
+
+    onSnap() {
+      if (this.buttonText === "J'adore") {
+        this.faceSnap.snaps++;
+        this.buttonText = "Burk";
+      } else {
+        this.faceSnap.snaps--;
+        this.buttonText = "J'adore"
+      }
+    }
+    
+
+    
 }
+
